8725fe30dd544c2688ce2e2b2ad9f50352cbb6e7
Bruno Luz <bsilva.se@gmail.com>
2021-01-09 22:55:49 -0300

    main - Estilizando ListTile

--- lib/trancacao/transacoes-list.dart
+++ lib/trancacao/transacoes-list.dart
@@ -1,6 +1,7 @@
 import 'package:app_expenses/helper/formatacao.dart';
 import 'package:app_expenses/trancacao/transacao.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TransacoesList extends StatelessWidget {
 
@@ -88,8 +89,63 @@ class TransacoesList extends StatelessWidget {
       itemCount: transacoesRealizadas.length,
       itemBuilder: (builderContext, index) {
         final transacao = transacoesRealizadas[index];
-        return itemTransacao(transacao);
-      },
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10.0),
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.black12,
+              )
+            )
+          ),
+          child: ListTile(
+            leading: Expanded(
+              child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.black38,
+                          width: 1
+                      ),
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(50.0)
+                      )
+                  ),
+                  child: Text(
+                    'R\$ ${ transacao.valor.toStringAsFixed(2) }',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+              ),
+            ),
+            title: Text(
+              transacao.titutlo,
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20.0
+              ),
+            ),
+            subtitle: Text(
+                Formatacao.dataDDMMYYYYHHMMSS(transacao.data),
+                style: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12.0
+                )
+            ),
+            trailing: IconButton(
+              color: Colors.redAccent,
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                print('Excluir!');
+              }
+            ),
+          ),
+        );
+      }
     );
   }
 
@@ -101,7 +157,7 @@ class TransacoesList extends StatelessWidget {
         child: transacoesRealizadas.isEmpty
           ? imagemNenhumaTransacao(context)
           : transacoesCadastradas(),
-      ),
+      )
     );
   }
 
